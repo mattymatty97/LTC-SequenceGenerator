c1f4e038372719ed78e5ecc8690e938ebae842f8
willis81808 <willis81808@gmail.com>
2024-01-06 20:22:07 -0700

    first commit

---
+++ Patches/ExecutionRecorder.cs
@@ -0,0 +1,144 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace SequenceGenerator.Patches;
+
+[HarmonyPatch]
+internal class ExecutionRecorder
+{
+    [Serializable]
+    public class ExecutionData
+    {
+        public List<ExecutionEvent> executionEvents = new List<ExecutionEvent>();
+    }
+
+    [Serializable]
+    public class ExecutionEvent
+    {
+        public string type;
+        public string method;
+
+        public ExecutionEvent(string type, string method)
+        {
+            this.type = type;
+            this.method = method;
+        }
+    }
+
+    internal static ExecutionData data = new ExecutionData();
+
+    private static Dictionary<string, List<string>> loggedAlready = [];
+
+    private static bool _recording = false;
+    public static bool Recording
+    {
+        get => _recording;
+        set
+        {
+            if (value)
+            {
+                data.executionEvents.Clear();
+                loggedAlready.Clear();
+            }
+            _recording = value;
+        }
+    }
+
+    static IEnumerable<MethodBase> TargetMethods()
+    {
+        var typesToPatch = typeof(StartOfRound).Assembly
+            .GetTypes()
+            .Where(t => !t.IsGenericType && !t.IsValueType && !t.ContainsGenericParameters && !t.IsAbstract)
+            .Where(t => t.IsSubclassOf(typeof(MonoBehaviour)) || t.IsSubclassOf(typeof(NetworkBehaviour)));
+
+        var methodsToPatch = typesToPatch
+            .SelectMany(t => t.GetMethods(BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+            .Where(m =>
+                !m.ContainsGenericParameters &&
+                !m.IsConstructor &&
+                !m.IsAbstract
+            );
+
+        Plugin.Log.LogWarning($"Found {methodsToPatch.Count()} methods to patch across {typesToPatch.Count()} types");
+
+        return methodsToPatch;
+    }
+
+    static void Prefix(object[] __args, MethodBase __originalMethod)
+    {
+        var type = __originalMethod.DeclaringType.Name;
+        var name = __originalMethod.Name;
+
+        if (Ignore(type, name)) return;
+
+        data.executionEvents.Add(new ExecutionEvent(__originalMethod.DeclaringType.Name, __originalMethod.Name));
+    }
+
+    static bool Ignore(string type, string name)
+    {
+        if (!Recording) return true;
+
+        if (loggedAlready.TryGetValue(type, out var methods))
+        {
+            var contains = methods.Contains(name);
+            if (contains)
+            {
+                return true;
+            }
+            else
+            {
+                methods.Add(name);
+            }
+        }
+        else
+        {
+            loggedAlready.Add(type, [name]);
+        }
+        return false;
+    }
+
+    public static string ExportData()
+    {
+        // Export JSON
+        string json = JsonConvert.SerializeObject(data, Formatting.Indented);
+        string jsonPath = Path.Combine(Paths.PluginPath, "raw.json");
+        File.WriteAllText(jsonPath, json);
+
+        // Export Mermaid Sequence Diagram
+        string mermaidDiagram = GenerateMermaidSequenceDiagram(data);
+        string mmdPath = Path.Combine(Paths.PluginPath, "sequenceDiagram.mmd");
+        File.WriteAllText(mmdPath, mermaidDiagram);
+
+        return mmdPath;
+    }
+
+    private static string GenerateMermaidSequenceDiagram(ExecutionData data)
+    {
+        var diagram = new StringBuilder();
+        diagram.AppendLine("sequenceDiagram");
+
+        string previousType = null;
+
+        foreach (var eventItem in data.executionEvents)
+        {
+            if (previousType != null && previousType != eventItem.type)
+            {
+                diagram.AppendLine($"    {previousType} -->> {eventItem.type}: ");
+                diagram.AppendLine($"    note over {eventItem.type}: {eventItem.type}<br/>{eventItem.method}");
+            }
+            else
+            {
+                diagram.AppendLine($"    note over {eventItem.type}: {eventItem.method}");
+            }
+
+            previousType = eventItem.type;
+        }
+
+        return diagram.ToString();
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,75 @@
+﻿using BepInEx.Logging;
+using LethalSettings.UI;
+using LethalSettings.UI.Components;
+using Mono.Security.Authenticode;
+using SequenceGenerator.Patches;
+using System;
+using System.Reflection;
+using UnityEngine;
+
+namespace SequenceGenerator;
+
+[BepInDependency("com.willis.lc.lethalsettings", BepInDependency.DependencyFlags.HardDependency)]
+[BepInPlugin(GeneratedPluginInfo.Identifier, GeneratedPluginInfo.Name, GeneratedPluginInfo.Version)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static ManualLogSource Log { get; private set; }
+
+    private void Awake()
+    {
+        Log = base.Logger;
+        
+        Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), GeneratedPluginInfo.Identifier);
+
+        var statusLabel = new LabelComponent
+        {
+            Text = ""
+        };
+        var recordButton = new ButtonComponent
+        {
+            Text = "Record",
+            OnClick = (self) =>
+            {
+                if (ExecutionRecorder.Recording)
+                {
+                    // on stop recording
+                    ExecutionRecorder.Recording = false;
+                    var outputMmdPath = ExecutionRecorder.ExportData();
+                    GUIUtility.systemCopyBuffer = outputMmdPath;
+                    self.Text = "Start Recording";
+                    self.Enabled = false;
+                    self.ShowCaret = false;
+                    statusLabel.Text = "Exporting...";
+                    GameNetworkManager.Instance.StartCoroutine(ExecuteAfter(2f, () =>
+                    {
+                        self.Enabled = true;
+                        self.ShowCaret = true;
+                        statusLabel.Text = "";
+                    }));
+                }
+                else
+                {
+                    // on start recording
+                    ExecutionRecorder.Recording = true;
+                    self.Text = "Stop Recording";
+                    statusLabel.Text = "Recording...";
+                }
+            }
+        };
+
+        ModMenu.RegisterMod(new ModMenu.ModSettingsConfig
+        {
+            Name = GeneratedPluginInfo.Name,
+            Version = GeneratedPluginInfo.Version,
+            Id = GeneratedPluginInfo.Identifier,
+            Description = "A tool for generating sequence diagrams of in-game code execution order",
+            MenuComponents = [ recordButton, statusLabel ]
+        }, true, true);
+    }
+
+    private static IEnumerator ExecuteAfter(float delay, Action action)
+    {
+        yield return new WaitForSeconds(delay);
+        action?.Invoke();
+    }
+}
